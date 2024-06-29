f024a8502a7a29dcf65eb21bc8c7aa491eb7b876
Rudolf Bárdos <bardos@outlook.com>
2024-06-29 19:37:55 +0200

    feat: add lambda stack stage

--- bin/aws-cdk-pipeline.ts
+++ bin/aws-cdk-pipeline.ts
@@ -5,22 +5,22 @@ import { AwsCdkPipelineStack } from '../lib/aws-cdk-pipeline-stack';
 
 const app = new cdk.App();
 new AwsCdkPipelineStack(app, 'AwsCdkPipelineStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
     env: {
         account: '130693061189',
         region: 'us-east-1'
     }
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
 
 app.synth();
--- lib/aws-cdk-pipeline-stack.ts
+++ lib/aws-cdk-pipeline-stack.ts
@@ -1,17 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './pipeline-app-stage';
 
 export class AwsCdkPipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('b10f/aws-cdk-pipeline', 'main'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
-    });
-  }
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'CdkPipeline',
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.gitHub('b10f/aws-cdk-pipeline', 'main'),
+                commands: ['npm ci', 'npm run build', 'npx cdk synth']
+            })
+        });
+
+        pipeline.addStage(new PipelineAppStage(this, 'test', {
+            env: {
+                account: '130693061189',
+                region: 'us-east-1'
+            }
+        }));
+    }
 }
